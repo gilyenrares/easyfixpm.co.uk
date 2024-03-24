8ea011c685904919ecf737bce8bd373dab744d56
gilyenrares <57135053+gilyenrares@users.noreply.github.com>
2024-03-24 22:39:40 +0000

    projects done

--- projects.php
+++ projects.php
@@ -138,7 +138,7 @@
   </div>
   <!-- ======= End project Item ======= -->
 
-<!-- ======= Hero Section ======= -->
+  <!-- ======= Hero Section ======= -->
   <section id="hero" class="s-hero mb-1">
     <div class="hero-container">
       <div data-aos="fade-up">
@@ -154,7 +154,7 @@
       </div>
     </div>
   </section>
-  <!-- End Hero -->
+  <!-- End Hero Section-->
 
   <!-- ======= Project Item ======= -->
   <div class="project-item" style="background-image: url(assets/img/projects/6/e3.jpg)">
@@ -302,11 +302,120 @@
     </div>
   </div>
   <!-- ======= End project Item ======= -->
+
   <!-- ======= Project Item ======= -->
   <div class="project-item" style="background-image: url(assets/img/projects/13/e2.jpg)">
     <div class="container" data-aos="fade-up">
       <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
         <h2 class="brand">Project thirteen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/14/e10.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project fourteen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/15/e2.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project fifteen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="s-hero">
+    <div class="hero-container">
+      <div data-aos="fade-in">
+        <h1 class="brand"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h1>
+        <h2 class="m-0">We operate in<span class="typed"
+            data-typed-items="South, West, North,  "></span> London</h2>
+        <p class="text-danger"><i class="bi bi-arrow-down-circle"></i> 
+        For more details contact us 
+        <i class="bi bi-arrow-down-circle"></i></p>
+        <div class="actions pb-2">
+          <a href="contact.php" class="btn btn-outline-danger">Contact</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/16/e2.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project sixteen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/17/e8.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project seventeen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/18/e2.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project eighteen</h2>
+        <i class="bi bi-house-up-fill text-success"> Residential</i>
+        <div class="row justify-content-between align-items-center pt-4">
+          <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
+          <h4 class="brand col-4 me-3"><span class="bg-light px-2"><b>Easy</b><i>Fix</i></span></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= End project Item ======= -->
+
+  <!-- ======= Project Item ======= -->
+  <div class="project-item" style="background-image: url(assets/img/projects/19/e3.jpg)">
+    <div class="container" data-aos="fade-up">
+      <div class="col-12 col-md-6 col-lg-4 project-item-details text-white rounded p-2 ps-3">
+        <h2 class="brand">Project nineteen</h2>
         <i class="bi bi-house-up-fill text-success"> Residential</i>
         <div class="row justify-content-between align-items-center pt-4">
           <a class="col-4 pb-1 text-danger" href="#">View more <i class="bi bi-arrow-up-right-square"></i></a>
