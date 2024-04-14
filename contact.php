24a7b1acd600489c0f754c3da0794002007f1c26
gilyenrares <57135053+gilyenrares@users.noreply.github.com>
2024-04-14 23:11:04 +0100

    quote done contact inc next

--- contact.php
+++ contact.php
@@ -107,20 +107,20 @@
         <!-- Contact form -->
         <div class="row justify-content-center mt-4">
           <div class="col-lg-9">
-            <form action="forms/contact.php" method="post" class="php-email-form">
+            <form action="assets/includes/quote.inc.php" method="post" class="php-email-form">
               <div class="row">
                 <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                 <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" value="<?php if (isset($_GET['name'])) {echo $_GET['name'];} ?>" required="required">
                 </div>
                 <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="text" class="form-control" name="tel" id="tel" placeholder="Telephone" required>
+                 <input type="phone" class="form-control" name="phno" id="phone" placeholder="Telephone"value="<?php if (isset($_GET['phno'])) {echo $_GET['phno'];} ?>" required="required">
                 </div>
               </div>
               <div class="form-group mt-3">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="<?php if (isset($_GET['email'])) {echo $_GET['email'];} ?>" required="required">
               </div>
               <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                 <textarea  class="form-control" name="description" id="description" placeholder="Message" rows="6" value="<?php if (isset($_GET['description'])) {echo $_GET['description'];} ?>" required="required"></textarea>
               </div>
               <div class="my-3">
                 <div class="loading">Loading</div>
